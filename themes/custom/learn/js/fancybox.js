fedf7a015118a0bfa6c617c647c296954238e131
wulff <wulff@ratatosk.net>
2011-11-19 21:11:25 +0100

    added link to full screenshot to non-mobile versions

--- themes/custom/learn/js/fancybox.js
+++ themes/custom/learn/js/fancybox.js
@@ -4,11 +4,15 @@ Drupal.behaviors.learnFancyBox = {
   attach: function (context, settings) {
     $('body').bind('responsivelayout', function(event, layouts) {
       if (layouts.to != 'mobile') {
-        $('a.fancybox', context).fancybox({
+        $('a.fancybox').fancybox({
           'padding'      : 0,
           'transitionIn' : 'none',
           'transitionOut': 'none',
           'overlayShow'  : false
+        });
+
+        $('a.fancybox').each(function () {
+          $(this).parent().addClass('fancybox').after('<p><a href="' + $(this).attr('href') + '">Screenshot</a></p>');
         });
       }
     });
