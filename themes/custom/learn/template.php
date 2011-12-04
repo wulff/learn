fc97a5709da3cfd795f3c5c1d19ef56576f2b2fc
wulff <wulff@ratatosk.net>
2011-12-04 14:49:21 +0100

    improved front page css, made rss text a link

--- themes/custom/learn/template.php
+++ themes/custom/learn/template.php
@@ -17,7 +17,7 @@
 function learn_feed_icon($variables) {
   $text = t('Subscribe to @feed-title', array('@feed-title' => $variables['title']));
   if ($image = theme('image', array('path' => 'misc/feed.png', 'width' => 16, 'height' => 16, 'alt' => $text))) {
-    return l($image, $variables['url'], array('html' => TRUE, 'attributes' => array('class' => array('feed-icon'), 'title' => $text))) . ' ' . t('Subscribe to RSS feed');
+    return l($image . ' ' . t('Subscribe to RSS feed'), $variables['url'], array('html' => TRUE, 'attributes' => array('class' => array('feed-icon'), 'title' => $text)));
   }
 }
 
